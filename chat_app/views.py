34669814d611b2a73fbfaf0eb8fdf9810cdefbe7
KKrntzk <sophie.kornetzki@gmail.com>
2026-05-26 14:08:47 +0200

    views updated with get and post request

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Chat
 
 # Create your views here.
+
+@csrf_exempt
+def chat_view(request):
+    if request.method == 'GET':
+        chats = list(Chat.objects.values('name', 'message', 'created_at'))
+        return JsonResponse(chats, safe=False)
+
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        new_chat = Chat.objects.create(
+            name=data['name'],
+            message=data['message']
+        )
+        return JsonResponse({'status': 'success', 'id': new_chat.id})
